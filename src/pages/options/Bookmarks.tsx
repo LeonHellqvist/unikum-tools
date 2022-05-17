b987139b9c3e709347fea464f3d0d6e0be2ff8c3
LeonHellqvist <leon.norlin@outlook.com>
2022-05-17 15:49:18 +0200

    saker

--- src/pages/options/Bookmarks.tsx
+++ src/pages/options/Bookmarks.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import "@pages/options/Options.css";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import {
   DragDropContext,
   Droppable,
@@ -41,52 +46,80 @@ function Bookmarks() {
 
   return (
     <Container maxWidth="md" sx={{ marginTop: 2 }}>
-      <Typography variant="h4" component="div">
+      <Typography variant="h4" component="div" sx={{marginBottom: 2}}>
         Bokmärken
       </Typography>
-      <Paper elevation={3} sx={{ width: "100%", height: "100%" }}>
-        <Box>
-          <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
-            <Droppable droppableId="droppable" direction="horizontal">
-              {(provided) => (
-                <Grid
-                  container
-                  spacing={2}
-                  style={{ height: "120px" }}
-                  className="droppable"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {bookmarks.map((bookmark, index) => {
-                    return (
-                      <Draggable
-                        key={bookmark.url}
-                        draggableId={bookmark.url}
-                        index={index}
-                      >
-                        {(provided) => (
-                          <div
-                            {...provided.draggableProps}
-                            ref={provided.innerRef}
-                            {...provided.dragHandleProps}
-                          >
-                            <Button
-                              variant="contained"
-                              color="primary"
-                              href={bookmark.url}
+      <Paper elevation={3} sx={{ width: "100%", height: "100%", paddingBottom: 2}}>
+        <Stack direction="column" divider={<Divider orientation="horizontal" flexItem />} spacing={1}>
+          <Box>
+            <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
+              <Droppable droppableId="droppable" direction="horizontal">
+                {(provided) => (
+                  <Grid
+                    container
+                    justifyContent="center"
+                    alignItems="center"
+                    className="droppable"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {bookmarks.map((bookmark, index) => {
+                      return (
+                        <Draggable
+                          key={bookmark.url}
+                          draggableId={bookmark.url}
+                          index={index}
+                        >
+                          {(provided) => (
+                            <div
+                              {...provided.draggableProps}
+                              ref={provided.innerRef}
+                              {...provided.dragHandleProps}
                             >
-                              {bookmark.title}
-                            </Button>
-                          </div>
-                        )}
-                      </Draggable>
-                    );
-                  })}
-                </Grid>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </Box>
+                              <Button
+                                sx={{margin:1, marginTop:2}}
+                                variant="contained"
+                                color="primary"
+                                href={bookmark.url}
+                              >
+                                {bookmark.title}
+                              </Button>
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </Grid>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </Box>
+          <Box>
+            <Grid container>
+              <Grid item xs={8} sx={{paddingLeft: 2, paddingRight: 1}}>
+                <Paper sx={{padding: 2}}>
+                  <Typography variant="h6" component="div" sx={{marginBottom: 2}}>
+                    Alternativ för individuellt bokmärke
+                  </Typography>
+                  <Divider/>
+                </Paper>
+              </Grid>
+              <Grid item xs={4} sx={{paddingLeft: 1, paddingRight: 2}}>
+                <Paper variant="outlined" sx={{padding: 2}}>
+                  <Typography variant="h6" component="div" sx={{marginBottom: 2}}>
+                    Alternativ för alla
+                  </Typography>
+                  <Divider/>
+                  <FormGroup>
+                    <FormControlLabel control={<Checkbox />} label="Samma färg" />
+                    <FormControlLabel control={<Checkbox />} label="Fin fisk" />
+                  </FormGroup>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Box>
+        </Stack>
       </Paper>
     </Container>
   );
