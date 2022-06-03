eaf84e7878cc036d7015e1b7f07f51bb5fbb1341
LeonHellqvist <leon.norlin@outlook.com>
2022-06-03 13:29:54 +0200

    Testare

--- src/pages/popup/Food.tsx
+++ src/pages/popup/Food.tsx
@@ -1,32 +1,20 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { v4 as uuidv4 } from "uuid";
-import Parser from 'rss-parser';
 import "./Popup.css";
 
-type CustomFeed = {foo: string};
-type CustomItem = {bar: number};
 
 interface CreateButtonProps {
   setPage: (params: any) => any;
 }
 // pass prop "setPage" to button with typescript
 const Food = ({ setPage }: CreateButtonProps) => {
-  const parser = new Parser();
 
   React.useEffect(() => {
-    parser.parseURL("https://skolmaten.se/duveholmsskolan/rss/weeks/").then(feed => {
-      console.log(feed);
-    });
-  }, [])
-
-
-  const openOptions = () => {
-    chrome.runtime.openOptionsPage();
-  };
+    fetch("https://skolmaten.se/duveholmsskolan/rss/weeks/", {mode: "no-cors"}).then(response => {
+      console.log(response.statusText);
+      return response.text();
+  })
+  .then(data => console.log(data))
+  }, []);
 
   return (
     <div className="App">
