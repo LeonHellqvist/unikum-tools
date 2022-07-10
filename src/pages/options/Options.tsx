c14ab45273fce01f2bc80497428988bc8030e247
LeonHellqvist <leon.norlin@outlook.com>
2022-07-10 10:30:28 +0200

    Lade till schema inställningar

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,43 +1,35 @@
 import React from "react";
 import "./Options.css";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import '@fontsource/roboto/400.css';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import "@fontsource/roboto/400.css";
 
 import Bookmarks from "./Bookmarks";
 import Food from "./Food";
-
-
-
+import Schedule from "./Schedule";
 
 const Options = () => {
-  const [page, setPage] = React.useState(0);
-
-  if (page === 0) {
-    return (  
-        <div className="App">
-          <header className="App-header">
-            <Box>
-              <AppBar position="static">
-                <Toolbar>
-                  <Typography variant="h6" component="div">
-                    Unikum tools
-                  </Typography>
-                </Toolbar>
-              </AppBar>
-            </Box>
-          </header>
-          <Bookmarks />
-          <Food />
-        </div>
-    );
-  }
-
   return (
-    <></>
-  )
+    <div className="App">
+      <header className="App-header">
+        <Box>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div">
+                Unikum tools
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </header>
+      <Bookmarks />
+      <Food />
+      <Schedule />
+      <Box sx={{ height: 40 }}></Box>
+    </div>
+  );
 };
 
 export default Options;
